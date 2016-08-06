8954879af40eed6457782532bb32ba320bac01e1
Eric M <ericmarchese77@gmail.com>
2016-08-06 16:36:59 -0400

    Removed gmail smtp settings from development/test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,17 +30,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    domain: "www.example.com",
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,17 +36,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    domain: "www.example.com",
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
